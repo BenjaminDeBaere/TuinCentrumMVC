bae0181bc3590cfac08e2e8853f5118aefe37c9d
BenjaminDeBaere <benjamin.de.baere1@gmail.com>
2017-02-23 11:26:19 +0100

    Eerste Commit

---
+++ MVC_TuinCentrum/Models/PlantProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_TuinCentrum.Models
+{
+    public class PlantProperties
+    {
+        [Display(ResourceType =typeof(Resources.Teksten), Name ="LabelPrijs")]
+        [Range(0,1000, ErrorMessageResourceType=typeof(Resources.Teksten), ErrorMessageResourceName="RangePrijs")]
+        public decimal VerkoopPrijs { get; set; }
+    }
+}
